83969228a104cd955dec2324404ed1cf1ec60b8e
csw3071 <csw30710@naver.com>
2025-09-03 17:26:14 +0900

    for react project second commit

--- src/components/ch03.component/component_separate.js
+++ src/components/ch03.component/component_separate.js
@@ -44,7 +44,7 @@ JSX 문법 규칙
 function App(){
     console.log('컴포넌트별 분리하기');
     return(
-        <div age = "20">
+        <div name = "김철수">
             <Top money = "100"/>
             <Content />
             <Bottom />
